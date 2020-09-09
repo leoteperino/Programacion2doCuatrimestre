187c440ec5e5f6a13699880c317ed72c235467cf
Leandro <leoteperino@gmail.com>
2020-09-09 08:21:20 -0300

    TP1 - Menu terminado

--- trabajoPractico1/src/trabajoPractico1.c
+++ trabajoPractico1/src/trabajoPractico1.c
@@ -15,8 +15,6 @@
 
 #define QTY_MIN -999999999
 #define QTY_MAX 999999999
-#define OPT_MENU_MIN 1
-#define OPT_MENU_MAX 5
 #define QTY_REINT 2
 
 int main(void) {
@@ -25,14 +23,86 @@ int main(void) {
 	float num_b = 0;
 	//Menu de Inicio
 	do{
-		printf("\n1 .Ingresar primer operando: (A=%.f)\n", num_a );
-		if(!utn_getInt(&opcionesMenu, "Ingrese la opcion de Menu deseada: ", "ERROR", OPT_MENU_MIN, OPT_MENU_MAX, QTY_REINT))
+		printf( "--Menu de opciones--\n"
+				"1. Ingresar primer operando (A=%.2f)\n"
+				"2. Ingresar Segundo operando (B=%.2f)\n"
+				"3. Calcular todas las operaciones:\n"
+				"   a) Calcular la suma (%.2f+%.2f)\n"
+				"   b) Calcular la resta (%.2f-%.2f)\n"
+				"   c) Calcular la division (%.2f/%.2f)\n"
+				"   d) Calcular la multiplicacion (%.2f*%.2f)\n"
+				"   e) Calcular el factorial (%.2f!)\n"
+				"4. Informar resultados\n",
+				num_a,
+				num_b,
+				num_a,
+				num_b,
+				num_a,
+				num_b,
+				num_a,
+				num_b,
+				num_a,
+				num_b,
+				num_a);
+		if(!utn_getInt(	&opcionesMenu,
+						"Ingrese la opcion de Menu deseada: ",
+						"ERROR",
+						QTY_MIN,
+						QTY_MAX,
+						QTY_REINT))
 		{
+			//Seteo de la opcion deseada
 			switch(opcionesMenu){
 					case 1:
-						printf("Aprestaste el uno\n");
+						printf("\n*********************************************\n");
+						printf("--Opcion 1--\n");
+						if(!utn_getFloat(	&num_a,
+											"Ingrese un numero: ",
+											"ERROR",
+											QTY_MIN,
+											QTY_MAX,
+											QTY_REINT))
+						{
+							printf("Su numero se cargo con exito!!\n");
+						}
+						else
+						{
+							printf("Algo salio mal, se acabaron tus reintentos\n");
+						}
+						printf("*********************************************\n\n");
+						break;
+					case 2:
+						printf("\n*********************************************\n");
+						printf("--Opcion 2--\n");
+						if(!utn_getFloat(	&num_b,
+											"Ingrese un numero: ",
+											"ERROR",
+											QTY_MIN,
+											QTY_MAX,
+											QTY_REINT))
+						{
+							printf("Su numero se cargo con exito!!\n");
+						}
+						else
+						{
+							printf("Algo salio mal, se acabaron tus reintentos\n");
+						}
+						printf("*********************************************\n\n");
+						break;
+					case 5:
+						printf("\n*********************************************\n");
+						printf("Gracias por usar la calculadora!!");
+						printf("\n*********************************************\n");
+						break;
+					default:
+						printf( "ERROR, Usted ingreso un numero incorrecto.\n"
+								"Vuelva a intentarlo.\n\n");
 						break;
 			}
+		}
+		else
+		{
+			printf("Algo salio mal, se acabaron tus reintentos\n\n");
 		}
 	}while(opcionesMenu != 5);
 	return EXIT_SUCCESS;
